0df27616ddee03d308235a9a71ec8ee949c31918
Aldar <aldarsemenov@mail.ru>
2023-09-03 11:20:07 +0800

    tailw

--- src/pages/MenuItemPage/index.js
+++ src/pages/MenuItemPage/index.js
@@ -1,15 +1,7 @@
 import { useParams } from "react-router-dom";
-import menu from "../../fakeData/menu";
+
 
 const MenuItemPage = () => {
-    const { id } = useParams();
-    let menuItem= menu.find((item)=>item.id==id)
-    return (
-    <> 
-    <h1>   {menuItem.name} </h1> 
-    <img className="  object-cover h-96 w-192" src={menuItem.image}></img>
-    <p>{menuItem.ing.join(", ")}</p>
-    </>
-    )
+  
 }
 export default MenuItemPage;
