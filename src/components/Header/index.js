9704f0fff28a181e0e89b1e054d3b0d389639acb
Aldar <aldarsemenov@mail.ru>
2023-09-26 09:38:26 +0800

    fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,23 +4,36 @@ import CartWidget from "../CartWidget"
 import CartProvider from "../providers/CartProvider"
 import { useDispatch } from "react-redux"
 import MenuCard from "../Menucard"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const Header = () => {
-    // useEffect(()=> {
-    //   window.addEventListener("scroll",scrollDistance)
-    //   return () => {
-    //     window.removeEventListener("scroll",scrollDistance)
-    //   }
-    // });
+
+
+      // const header = document.querySelector('.header');
+      // const first = document.querySelector('.first');
+    
+
   
- 
+
+      //   window.addEventListener("scroll",()=> {
+      //     let scrollDistance = window.scrollY;
+      //     if (scrollDistance>=240)
+      //     {
+      //       header.classList.add('header--fixed');
+            
+      //     } else {
+      //       header.classList.remove('header--fixed');
+            
+      //     }
+
+      //   });
+       
     return (
         
-
-        <div className="shadow-lg my-2 text-xl mx-auto   w-full grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5   flex   ">
+      
+       <div className=" shadow-lg my-2 text-xl mx-auto   w-full grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5   flex   ">
             {headercategory.map((item) => (
-            <div className=" text-2xl hover:bg-slate-700  container align-middle text-center rounded-xl p-2 hover:text-white  transition border-6 border-indigo-500">
+            <div className="text-2xl item-center hover:bg-slate-700  container align-middle text-center rounded-xl p-2 hover:text-white  transition border-6 border-indigo-500">
                 <Link to={`/catalog/${item.categ}`}>
                     
 
@@ -34,6 +47,6 @@ const Header = () => {
           </CartProvider>
           
         </div>
-    )
+          )
 }
 export default Header
