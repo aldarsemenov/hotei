26be7c9e5adbf05b82544873c05232df1ac0f0f9
Aldar <aldarsemenov@mail.ru>
2023-09-26 10:01:59 +0800

    widget

--- src/components/CartWidget/index.js
+++ src/components/CartWidget/index.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 const CartWidget = ({ cart, sum }) => {
     const dispatch = useDispatch();
     return (
-      <div className=" hover:bg-slate-700 transition box-border w-32 justify-center  container flex w-40 text-center flex   rounded-xl">
+      <div className="  hover:bg-slate-700 transition box-border w-32 justify-center  container flex w-40 text-center flex   rounded-xl">
         <Link to ="/cart">
         <div className="inline-flex w-32 justify-center py-3 flex-wrap">
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -34,10 +34,13 @@ const Header = () => {
        <div className=" shadow-lg my-2 text-xl mx-auto   w-full grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5   flex   ">
             {headercategory.map((item) => (
             <div className="text-2xl item-center hover:bg-slate-700  container align-middle text-center rounded-xl p-2 hover:text-white  transition border-6 border-indigo-500">
+                <button>
                 <Link to={`/catalog/${item.categ}`}>
+                    {item.name}</Link> 
+
+                </button>
                     
 
-                    {item.name}</Link> 
             </div>
             ))
 
